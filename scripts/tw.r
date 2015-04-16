e2a0b0cbc0dc96cc8c36a1cb128a861220f338e4
jpgerke <justin.gerke@gmail.com>
2015-04-15 21:04:40 -0700

    modified tw code, but no testing done yet

--- scripts/tw.r
+++ scripts/tw.r
@@ -1,57 +1,55 @@
-Mmake<-function(data){
-	pcdat<-data
-	mv<-apply(pcdat,2,mean,na.rm=TRUE)
-	#make vector of sqrt(p*(1-p))
-	vv<-mv/2
-	vv<-sqrt(vv*(1-vv))
-	#redo data and vectors to take out monomophic sites
-	dat<-pcdat[,vv>0&is.na(vv)==FALSE]
-	mv<-mv[vv>0&is.na(vv)==FALSE]
-	vv<-vv[vv>0&is.na(vv)==FALSE]
-	M<-t((t(dat)-mv)/vv)
-	M[is.na(M)]<-0
-	return(M)	
-}
-	
-TWcalc<-function(dat,k){
+
+#eigs = eigenvalues;   n.ind = number of individuals; k = number of axes to test
+TWcalc<-function(eigs, n.inds, k){
+	#make sure eigenvalues are sorted
+	eigs = sort(eigs, decr=T)
+	#load a table for test statistics
 	test<-read.table("twtable.txt", header=FALSE)
-	x<-dat
-	y<-x%*%t(x)
-	eig<-eigen(y)
-	eig<-eig$values
-	eig<-eig[1:(length(eig))]
-	n<-ncol(x)
-	m0=nrow(x)
+	m0=n.inds
 	k=k
+	# calculate the statistic for dimensions m0
+	# equations are from Patterson et al. 2006
 	TWres_<-c()
 	for (i in 1:k){
 		m=(m0)-i+1
 		m=m
-		eiv<-eig[i:(m-1)]
-		#n_=n
-		n_=((m+1)*sum(eiv)^2)/((m-1)*sum(eiv^2)-sum(eiv)^2)
-		S=((sqrt(n_-1)+sqrt(m)))/n_*(1/sqrt(n_-1)+1/sqrt(m))^(1/3)
-		u_=(sqrt(n_-1)+sqrt(m))^2/n_
+		#use the eigenvalues from k to mprime (step 3 of "a test for population structure")
+		eiv<-eigs[i:(m-1)]
+		#nprime is equation 10
+		nprime=((m+1)*sum(eiv)^2)/((m-1)*sum(eiv^2)-sum(eiv)^2)
+		#S is equation 6
+		S=((sqrt(nprime-1)+sqrt(m)))/nprime*(1/sqrt(nprime-1)+1/sqrt(m))^(1/3)
+		#u_ is equation 5
+		u_=(sqrt(nprime-1)+sqrt(m))^2/nprime
+		#L1 is step 5 in "a test for population structure"
 		L1<-(m-1)*eiv[1]/sum(eiv)
+		#the test statistic: equation 7
 		TW_<-(L1-u_)/S
+		# collate test statistics
 		TWres_<-c(TWres_,TW_)
 	}
-
-	#TWres_
+	
+	#calculate p values
+	if (require(RMTstat)){
+		pvals = c()
+		for (i in 1:length(TWres_)){
+			p = ptw(TWres[i], lower.tail=F)
+			pvals = c(pvals, p)
+		}
+	}
+	else{
+		pvals = NA
+	}
 	pres<-c()
 	for (i in 1:length(TWres_)){
 		TW<-TWres_[i]
 		dif<-abs(test[,1]-TW)
 		p<-test[dif==min(dif),2]
 		pres<-c(pres,p)
+		}
 	}
-	#pres
-	res<-list(TWres_,pres)
+	res<-list(TWstats=TWres_, Sig=pres, pval=pvals)
 	return(res)
 }
 
-#usage, data as 0,1 and 2s
-M<-Mmake(dat)
-TWcalc(M, 10)
-TWcalc(M, 25)
 
